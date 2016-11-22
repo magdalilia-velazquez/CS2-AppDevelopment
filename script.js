5b5ebc43787905d2334d6d723635da97501a458e
magdalilia-velazquez <magdalilia.velazquez@speeracademy.org>
2016-11-22 11:51:57 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+        $('h2').mouseenter(function() {
+                $('h2').fadeTo('fast',1.00);
+    });
+        $('h2').mouseleave(function() {
+                $('h2').fadeTo('slow',0.5);
+    });
+});
+
+$(document).ready(function() {
+        $('h1').click(function() {
+                $('h1').fadeOut('fast',1.00);
+        });
+});
+
+$(document).ready(function() {
+        $('div').click(funtion() {
+                $('this').fadeOut('fast');
+        });
+)};
